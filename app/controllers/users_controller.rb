3dbcd29e0223bc8617d1da1db22258a7902c8241
Anne Jhetson & Chris Lind <student@epicodus.com>
2014-04-09 11:56:32 -0700

    users can upload their avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to ("/users/#{@user.id}")
+    else
+      render 'edit'
+    end
+  end
+
+
   private
     def user_params
-      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+      params.require(:user).permit(:email, :name, :password, :password_confirmation, :avatar)
     end
 end
